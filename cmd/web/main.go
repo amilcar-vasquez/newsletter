2ae18a5d0568235c470be884fde924d6603be66f
Amilcar Vasquez <2022156707@ub.edu.bz>
2025-02-18 10:08:04 -0600

    added handler for home

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+//handler function for home
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to our newsletter"))
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	// mux is our router
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
