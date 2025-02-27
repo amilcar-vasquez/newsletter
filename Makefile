41e97defdc51648a46a444a6a2beafec744d3e26
Amilcar Vasquez <2022156707@ub.edu.bz>
2025-02-27 09:47:08 -0600

    added tmp file

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ run-app:
 	@echo "Running the app..."
 	@go run ./cmd/web -addr=":3000"
 
-.PHONY: run/test
-run/test:
+.PHONY: run-test
+run-test:
 	@echo "Running the tests..."
 	@go test -v ./...
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,8 @@ package main
 
 import (
 	"flag"
+	"html/template"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
@@ -12,7 +14,24 @@ import (
 //handler function for home
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to our newsletter"))
+	tmpl, err := template.ParseFiles("ui/html/home.page.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	data := map[string]string{
+		"Title":    "Tuesday | Thursday",
+		"Greeting": "Mawnin",
+		"Message":  "It's a great day",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 // about page handler
